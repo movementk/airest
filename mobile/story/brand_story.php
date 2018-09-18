38577b67ce2a9ae9a22a6613e0bdc1ec9626068a
kimyh5392 <mk.kimyh@gmail.com>
2018-09-18 18:06:05 +0900

    모바일 브랜드 스토리 오나료.

--- mobile/story/brand_story.php
+++ mobile/story/brand_story.php
@@ -23,10 +23,52 @@
     <!-- Sub Content Start -->
     <main id="content" class="brand-story">
         <section class="section-1">
-            
+            <div class="container">
+                <div class="section-header">
+                    <h3>
+                        지친 일상에서의<br>
+                        아주 작은 여유,<br>
+                        에어레스트는<br>
+                        작지만 확실한 여유를<br>
+                        만들어드리기 위해<br>
+                        노력합니다
+                    </h3>
+                </div>
+                <div class="summary">
+                    <p class="effect-txt-1">공부, 아르바이트, 일, 취미, 운동 등<br>지금 우리는 너무나도 복잡하고 바쁜 하루를 보내고 있습니다</p>
+                    <strong>그 속에서 정작 나를 위한 시간은 <br>얼마나 될까요?</strong>
+                    <p class="effect-txt-1">
+                        사람들을 행복하게 만드는 노력,<br>
+                        그 안에서 숨은 가치를 만들어내는 일<br>
+                        당신이 원하는 모든 곳에서 부담 없이<br>
+                        에어레스트 할 수 있도록 꼭 필요한 기능에<br>
+                        최대의  성능을 담았습니다
+                    </p>
+                </div>
+            </div>
+            <div class="section-content">
+                <figure>
+                    <img src="/mobile/assets/images/story/img_story01.jpg" class="img-fluid" alt="">
+                </figure>
+                <figure>
+                    <img src="/mobile/assets/images/story/img_story02.jpg" class="img-fluid" alt="">
+                </figure>
+            </div>
         </section>
-        <section class="section-1">
-            
+        <section class="section-2">
+            <div class="container">
+                <div class="details">
+                    <h4>
+                        당신의 삶을 위하여<br>
+                        <strong>에어레스트 하세요</strong>
+                    </h4>
+                    <div class="btn-area">
+                        <p>
+                            <a href="#" class="btn btn-more" role="button">제품 보러가기</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 
@@ -41,6 +83,14 @@
                 }, 300);
             });
             
+            // section effect
+            $(window).on('scroll', function() {
+                $('.section-header, .section-content > figure, .summary, .section-2').each(function(index, elem) {
+                    if ($(window).scrollTop() > $(elem).offset().top - ($(window).height() / 1.3)) {
+                        $(elem).addClass('action');
+                    }
+                });
+            });
         })(jQuery);
     </script>
 </body>
