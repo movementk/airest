0961ab9b2ff9f217f0439d321ffdc1b7edecf4c6
kimyh5392 <mk.kimyh@gmail.com>
2018-11-13 12:32:36 +0900

    개인정보처리방침 완료.

---
+++ customer/privacy.php
@@ -0,0 +1,32 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/customer.css">
+</head>
+<body class="sub customer">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="container">
+            <h2 class="page-title">개인정보처리방침</h2>
+        </div>
+    </div>
+    
+    <!-- Sub Content Start -->
+    <main id="content">
+        <div class="container">
+            <section class="privacy">
+                <div class="section-header">
+                    <h3 class="section-title">개인정보처리방침</h3>
+                </div>
+                <div class="section-content">
+                    <article class="terms">
+                        개인정보처리방침 내용
+                    </article>
+                </div>
+            </section>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ mobile/customer/privacy.php
@@ -0,0 +1,26 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/mobile/assets/css/sub.css">
+<link rel="stylesheet" href="/mobile/assets/css/customer.css">
+</head>
+<body class="sub customer">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <!-- Sub Content Start -->
+    <main id="content">
+        <div class="container">
+            <section class="privacy">
+                <div class="section-header">
+                    <h2>개인정보처리방침</h2>
+                </div>
+                <div class="section-content">
+                    <article class="terms">
+                        개인정보처리방침 내용
+                    </article>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
